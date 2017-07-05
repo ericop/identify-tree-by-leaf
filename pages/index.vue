9d6638ac066c94cff99ece497e264f789365def9
EricOP <ericop@users.noreply.github.com>
2017-07-05 09:12:54 -0400

    added wiki media Leaf_morphology image

--- pages/index.vue
+++ pages/index.vue
@@ -10,8 +10,13 @@
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+        <a href="https://github.com/ericop/identify-tree-by-leaf" target="_blank" class="button--grey">GitHub</a>
       </div>
+      <a 
+      title="By derivative work: McSush (talk) Leaf_morphology_no_title.png: User: Debivort (Leaf_morphology_no_title.png) [GFDL (<a href=&quot;http://www.gnu.org/copyleft/fdl.html&quot;>http://www.gnu.org/copyleft/fdl.html</a>) or CC-BY-SA-3.0 (<a href=&quot;http://creativecommons.org/licenses/by-sa/3.0/&quot;>http://creativecommons.org/licenses/by-sa/3.0/</a>)], via Wikimedia Commons" 
+      href="https://commons.wikimedia.org/wiki/File%3ALeaf_morphology.svg"><img width="512" alt="Leaf morphology" 
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Leaf_morphology.svg/512px-Leaf_morphology.svg.png"/>
+      </a>
     </div>
   </section>
 </template>
